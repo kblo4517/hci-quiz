c6838cbaff017932aaf6e8ee346cf48573a19101
Kavon Blossom <50221450+kblo4517@users.noreply.github.com>
2021-04-23 21:03:08 -0600

    t

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import Home from './Home'
 import FirstTask from './FirstTask';
 import SecondTask from './SecondTask'
